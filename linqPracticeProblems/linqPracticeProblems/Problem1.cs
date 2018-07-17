57ec85ef5e1b82ea493c3e5bbbd9716fd200e1bd
Jacob Jahneke <jacobjahneke@gmail.com>
2018-07-17 11:04:53 -0500

    problem one work

---
+++ linqPracticeProblems/linqPracticeProblems/Problem1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linqPracticeProblems
+{
+    class Problem1
+    {
+       	public static List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+        public static void SearchWord()
+        {
+            var substringList =
+                        from m in words
+                        where m.Contains("th")
+                        select m;
+            foreach (var word in substringList)
+            {
+                Console.WriteLine(word);
+            }
+            Console.ReadLine();
+        }
+       
+    }
+}
---
+++ linqPracticeProblems/linqPracticeProblems/Problem2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linqPracticeProblems
+{
+    class Problem2
+    {
+        //List<string> names = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike" };
+        //public void SearchList()
+        //{
+
+        //    var duplicateRemovedList =
+        //               from m in names
+        //               where m.DistinctBy("")
+        //               select m;
+        //}
+       
+    }
+}
